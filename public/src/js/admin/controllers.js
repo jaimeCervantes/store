be49fa53b00c5a0e014d08df27b9286194eaa0f7
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2016-10-03 16:24:28 -0500

    Deleting angular files, here we are going to use polymer

--- public/src/js/admin/controllers.js
+++
@@ -1,2 +0,0 @@
-module.exports.productsCtrl = require('./controllers/products.js');
-module.exports.newProductCtrl = require('./controllers/newProduct.js');
--- public/src/js/dependencies.js
+++
@@ -1,5 +0,0 @@
-require('angular');
-require('angular-route');
-require('angular-animate');
-require('angular-material');
-require("angular-aria");
